e2ad2f29e7405c1f8cae1ae8afece754d33c3df3
Sebastian <40932170+Nazaled@users.noreply.github.com>
2019-09-25 08:34:29 +0200

    Update serversafe.sh

--- serversafe.sh
+++ serversafe.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 clear
-read -p"Server jetzt absichern? (j/n) " response
+read -p "Server jetzt absichern? (j/n) " response
 
 if [ "$response" == "j" ]; then
 # Server Update und Upgrade
     sudo apt update && sudo apt upgrade -y
 # Apticron installieren
     sudo apt install apticron -y
-echo "Ändern Sie ihre E-Mail Adresse für Apticron \"EMAIL=deine@mailadresse.de\". Weitere mit Enter..." response
+echo -p "Ändern Sie ihre E-Mail Adresse für Apticron \"EMAIL=deine@mailadresse.de\". Weitere mit Enter..."
     sudo nano /etc/apticron/apticron.conf
 clear
 # SSH Root Login verbieten
@@ -15,7 +15,7 @@ echo "SSH Root Login wird geschlossen. Welcher Benutzer soll neu angelegt werden
   read newuser
   sudo adduser $newuser
   sudo usermod -aG sudo $newuser
-echo "Suchen Sie nun PermitRootLogin yes und ändern Sie dieses auf PermitRootLogin no. Weitere mit Enter..." response
+echo -p "Suchen Sie nun PermitRootLogin yes und ändern Sie dieses auf PermitRootLogin no. Weitere mit Enter..." response
   sudo nano /etc/ssh/sshd_config
   sudo reboot now
 
