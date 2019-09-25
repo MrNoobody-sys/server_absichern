518fb7e46e01a3ee69748fe09c9ddc42c4be33e3
Sebastian <40932170+Nazaled@users.noreply.github.com>
2019-09-25 07:28:49 +0200

    Update serversafe.sh

--- serversafe.sh
+++ serversafe.sh
@@ -3,5 +3,18 @@ clear
 read -p"Server jetzt absichern? (j/n) " response
 
 if [ "$response" == "j" ]; then
+# Server Update und Upgrade
+    sudo apt update && sudo apt upgrade -y
+# Apticron installieren
+    sudo apt install apticron -y
+    sudo nano /etc/apticron/apticron.conf
+clear
+# SSH Root Login verbieten
+echo "SSH Root Login wird geschlossen. Welcher Benutzer soll neu angelegt werden?"
+  read newuser
+  sudo adduser $newuser
+  sudo usermod -aG sudo $newuser
+echo "Suchen Sie nun PermitRootLogin und ändern Sie dieses auf PermitRootLogin no" response
+  sudo nano /etc/ssh/sshd_config
 
 fi
